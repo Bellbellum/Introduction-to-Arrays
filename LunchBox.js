6d1683b0ccd81936a815d56b6f7b1d5092b75cf1
Sarah K <63137575+Bellbellum@users.noreply.github.com>
2025-08-05 20:59:30 -0700

    Create LunchBox.js

---
+++ LunchBox.js
@@ -0,0 +1,18 @@
+class LunchBox {
+  constructor(name) {
+    this.name = name;
+    this.inventory = [];
+  }
+
+  addItem(item) {
+    // Put your code here to add an item to the inventory
+  }
+
+  removeItem(item) {
+    // Put your code here to remove an item to the inventory
+  }
+
+  listItem() {
+    // Put your code here to print all items in the inventory
+  }
+}
